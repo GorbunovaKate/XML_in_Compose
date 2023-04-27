51bb934132d0ceed58a41ff649068abf1415005e
Gorbunova Ekaterina <login.kate1.pass@gmail.com>
2023-04-27 21:24:17 +0700

    can change size

--- app/src/main/java/com/gorbunova/xml_in_compose/MainActivity.kt
+++ app/src/main/java/com/gorbunova/xml_in_compose/MainActivity.kt
@@ -7,13 +7,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.gorbunova.xml_in_compose.ui.theme.XML_in_ComposeTheme
-import com.judemanutd.katexview.KatexView
+import ru.noties.jlatexmath.JLatexMathView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,27 +38,10 @@ fun XML_in() {
         factory = {
             View.inflate(it,R.layout.text_layout, null)
         },
-    modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize(),
         update ={
-            val exempel = it.findViewById<KatexView>(R.id.katex_text)
-            exempel.setText(text)
+            val exempel = it.findViewById<JLatexMathView>(R.id.math_view)
+            exempel.setLatex(text)
         }
     )
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    XML_in_ComposeTheme {
-        Greeting("Android")
-    }
 }
